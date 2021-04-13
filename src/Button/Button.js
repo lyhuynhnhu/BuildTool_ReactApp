60390d7ad0749a25188f462b435bb17dfc491f19
lyhuynhnhu <lyhuynhnhu200699@gmail.com>
2021-04-13 15:01:01 +0700

    add scss

---
+++ src/Button/Button.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Button.scss';
+
+function Button() {
+    return (
+        <button className="btn-control">Click here</button>
+    )
+}
+
+export default Button;
